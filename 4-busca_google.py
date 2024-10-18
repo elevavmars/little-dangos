88fa5a52b47f02398aaf6dfceb9914ff3380d2b5
mars <marcus.metelis@outlook.com>
2024-10-18 08:30:23 +0100

    ow

--- 4-busca_google.py
+++ 4-busca_google.py
@@ -3,6 +3,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+# Termo de pesquisa
+term = input('Digite o que deseja pesquisar:\n')
+
 # Iniciando Driver
 browser = webdriver.Firefox()
 browser.get('https://www.google.com/')
+
+# Encontrando o elemento
+elem = browser.find_element(
+    By.XPATH,
+    "//textarea[@aria-label='Search']"
+)
+
+# Enviando termo para pesquisa
+elem.send_keys(term)
+elem.send_keys(Keys.ENTER)
